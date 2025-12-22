1ff1bae12493d7b1d1ffcb8daa53efdcbad4dee8
IanHewlett <ian.w.hewlett@gmail.com>
2025-12-22 17:39:35 -0500

    add main

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    textnode = TextNode("This is some anchor text", TextType("link"), "https://www.boot.dev")
+    print(textnode)
+
+if __name__ == "__main__":
+    main()
